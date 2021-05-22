873d5375f5a9f312f02676a2e7411d5a462a4e23
koryun23 <koryun.grigoryan.5@gmail.com>
2021-05-22 16:19:50 +0400

    getting the words translations

--- main.py
+++ main.py
@@ -14,13 +14,40 @@ class MainApp(App):
 class MainPage(Screen):
     def word_translate(self, word):
 
-        url = "https://www.lingvolive.com/en-us/"+"translate/en-ru/"+word.lower()
+        url = "https://www.lexico.com/definition/"+word.lower()
         print(url)
-        # r = requests.get(url)
-        # c = r.content
-        # soup = BeautifulSoup(c, "html.parser")
+        r = requests.get(url)
+        c = r.content
+        soup = BeautifulSoup(c, "html.parser")
         # print(soup.find_all("ol", {"class":"_1Mc81"}))
+        types = soup.find_all("section", {"class":"gramb"})
+        if not types:
+            print("No exact matches for word %s" %(word))
+            print("Here are the nearest results")
+            results = soup.find("ul", {"class": "search-results unpadded"}).find_all("li")
+            for result in results:
+                print(result.find("a").text)
 
+        if len(types) > 4:
+            limit = 4
+        else:
+            limit = len(types)
+        for i in range(limit):
+            deffinition_type = types[i].find("h3").text
+            p = types[i].find_all("p")
+            for item in p:
+                deffinition = item.find("span", {"class":"ind"})
+                if deffinition:
+                    print(deffinition_type, deffinition.text)
+            
+
+        # for i in range(len(types)):
+        #     print(i+1, types[i].text)
+
+
+
+            
+            
 
 if __name__ == "__main__":
     MainApp().run()
