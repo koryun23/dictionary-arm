28640bf74958767ce98fb9cbb2dd0f1f4f2aa2b2
koryun23 <koryun.grigoryan.5@gmail.com>
2021-05-27 18:45:25 +0400

    changing the import statement(while importing bs4)

--- scraper.py
+++ scraper.py
@@ -1,5 +1,26 @@
 import requests
 import re
+import imp
+import sys
+
+class ImportBlocker(object):
+
+    def __init__(self, *args):
+        self.black_list = args
+
+    def find_module(self, name, path=None):
+        if name in self.black_list:
+            return self
+
+        return None
+
+    def load_module(self, name):
+        module = imp.new_module(name)
+        module.__all__ = [] # Necessary because of how bs4 inspects the module
+
+        return module
+
+sys.meta_path = [ImportBlocker('bs4.builder._htmlparser')]
 from bs4 import BeautifulSoup
 
 def word_translate(word):
